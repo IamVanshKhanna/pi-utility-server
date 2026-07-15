a1dbff39a58dd0f4ca84667fe011c3d24f3b551d
vansh <vansh@homelab.local>
2026-07-15 13:34:36 +1000

    clean: remove _retired/ from working tree, preserved in git history (see commit 8facdd02)

--- _retired/windows/config/alertmanager/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sed -e "s|\${TELEGRAM_BOT_TOKEN}|$TELEGRAM_BOT_TOKEN|g" -e "s|\${TELEGRAM_CHAT_ID}|$TELEGRAM_CHAT_ID|g" /etc/alertmanager/alertmanager.yml > /tmp/alertmanager.yml
-exec /bin/alertmanager --config.file=/tmp/alertmanager.yml --storage.path=/alertmanager
